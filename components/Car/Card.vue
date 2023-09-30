3f3a837f4c3f9fc5e24272285027d084c65d5360
Yusuf ÇOBAN <pyusuf24@gmail.com>
2023-10-01 00:36:36 +0300

    Add files via upload
    added vueuse/nuxt and image/nuxt

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,27 +1,40 @@
 <template>
-	<!-- CAR CARD -->
-	<div
-		class="shadow border min-w-[32rem] overflow-hidden mb-5 cursor-pointer h-[200px]"
-		@click="navigateTo(`/car/${car.name}-${car.id}`)"
-	>
-		<div class="flex h-full">
-			<img :src="car.url" alt="" class="w-[300px] h-full" />
-			<div class="p-4 flex flex-col min-w-[5rem]">
-				<div class="">
-					<h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
-					<p class="text-gray-700">
-						{{ car.description }}
-					</p>
-				</div>
-				<h1 class="mt-auto text-xl">${{ car.price }}</h1>
-			</div>
-		</div>
-	</div>
-	<!-- CAR CARD -->
+  <div
+    class="relative shadow border min-w-[32rem] overflow-hidden mb-5 cursor-pointer h-[200px]"
+  >
+    <img
+      @click="emit('favor', car.id)"
+      class="absolute w-7 right-5 top-2 z-20"
+      :src="favored ? heartFilled : heartOutline"
+      alt="img"
+    />
+    <div @click="navigateTo(`/car/${car.name}-${car.id}`)" class="flex h-full">
+      <NuxtImg :src="car.url" alt="" class="w-[300px] h-full" />
+      <div class="p-4 flex flex-col min-w-[5rem]">
+        <div class="">
+          <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
+          <p class="text-gray-700">
+            {{ car.description }}
+          </p>
+        </div>
+        <h1 class="mt-auto text-xl">${{ car.price }}</h1>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import heartFilled from "@/assets/heartFilled.png";
+import heartOutline from "@/assets/heartOutline.png";
+
 const props = defineProps({
-	car: Object,
+  car: Object,
+  favored: Boolean,
 });
+
+//const favored = useState(`favored-${props.car.id}`, () => {
+//  return false;
+//});
+
+const emit = defineEmits(["favor"]);
 </script>
--- components/Car/Detail.vue
+++ components/Car/Detail.vue
@@ -1,22 +1,22 @@
 <template>
-	<div class="mt-10">
-		<img :src="car.url" class="w-full" alt="" />
-		<h1 class="mt-10 text-4xl">{{ car.name }}</h1>
-		<div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
-			<div class="flex">
-				<p class="mr-2">{{ car.seats }} seats</p>
-				<p class="mr-2">|</p>
-				<p class="mr-2">{{ car.miles }} miles</p>
-			</div>
-			<div>
-				<p class="font-bold text-2xl">${{ car.price }}</p>
-			</div>
-		</div>
-	</div>
+  <div class="mt-10">
+    <NuxtImg :src="car.url" class="w-full" alt="" />
+    <h1 class="mt-10 text-4xl">{{ car.name }}</h1>
+    <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
+      <div class="flex">
+        <p class="mr-2">{{ car.seats }} seats</p>
+        <p class="mr-2">|</p>
+        <p class="mr-2">{{ car.miles }} miles</p>
+      </div>
+      <div>
+        <p class="font-bold text-2xl">${{ car.price }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 const props = defineProps({
-	car: Object,
+  car: Object,
 });
 </script>
